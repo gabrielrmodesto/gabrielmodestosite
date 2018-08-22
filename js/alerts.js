dacfdecf55df7bc2415c143b60d50bf2ab7cf989
GabrielRM <gabrielrmodesto5@gmail.com>
2018-08-22 17:46:14 -0300

    Arquivo de alerta implementado para sweet alert

---
+++ js/alerts.js
@@ -0,0 +1,9 @@
+var btn = document.querySelector(".btnEnviar");
+
+btn.addEventListener("click", function(){
+	swal({
+		title: "Muito obrigado",
+		text: "Logo irei responder sua mensagem",
+		button: "OK"
+	});
+})
