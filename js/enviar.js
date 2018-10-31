7b94820f75f3ccf5549151eca6c94c4db25eb5cc
GabrielRM <gabrielrmodesto5@gmail.com>
2018-10-31 17:30:16 -0300

    Função para envio de email

---
+++ js/enviar.js
@@ -0,0 +1,7 @@
+function enviarEmail(){
+	var link = "mailto:gabrielrmodesto5@gmail.com"
+			 + "&subject=" + escape("Mensagem via site")
+			 + "&body=" + escape(document.getElementById('mensagemPessoa').value);
+
+	window.location.href = link; 
+}
