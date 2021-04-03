07a5444267133ee1ebfb2c6aa41bd6623bd996fc
Gabriel Modesto <gabrielrmodesto5@gmail.com>
2021-04-03 00:44:43 -0300

    Fix my package in scripts

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,13 +2,13 @@ import Head from 'next/head';
 import styles from '../styles/pages/Home.module.css';
 
 function Home() {
-    return (   
-        <div className={styles.container}>
-            <Head>
-                <title>Gabriel Modesto</title>
-            </Head>
-        </div>
-    );
+	return (
+		<div className={styles.container}>
+			<Head>
+				<title>Gabriel Modesto</title>
+			</Head>
+		</div>
+	);
 }
 
 export default Home;
