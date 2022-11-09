dd40e1ec38dc68d3f772891fe8f514ea67282d99
Gabriel Modesto <gabrielrmodesto5@gmail.com>
2022-11-09 00:22:20 -0300

    Section home com efeito de typing

--- js/animate-text.js
+++ js/animate-text.js
@@ -1,15 +1,12 @@
-const text = document.querySelector('#segunda-parte-texto');
-const loadText = () => {
-  setTimeout(() => {
-    text.textContent = "Desenvolvedor";
-  }, 0);
-  setTimeout(() => {
-    text.textContent = "Pai";
-  }, 4000);
-  setTimeout(() => {
-    text.textContent = "Rugbier";
-  }, 8000);
-}
-
-loadText();
-setInterval(loadText, 12000);
+var text = new Typed('span.segunda-parte-texto', {
+  strings: [
+    'Desenvolvedor', 
+    'Rugbier', 
+    'Pai',
+    'Tecnólogo'
+  ],
+  typeSpeed: 100,
+  backSpeed: 50,
+  backDelay: 2000,
+  loop: true
+});
